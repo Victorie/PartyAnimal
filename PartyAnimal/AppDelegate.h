8e29ce283d575616a49f3eba071d4869bc1c9a1f
msc0953 <msc0953@gmail.com>
2014-07-02 16:54:32 +0800

    New Project for PartyAnimal

---
+++ PartyAnimal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PartyAnimal
+//
+//  Created by 李偉誠 on 7/2/14.
+//  Copyright (c) 2014 Victoire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
