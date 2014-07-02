40e7bc91e320786de02c13f84036dc324fe5fdd1
msc0953 <msc0953@gmail.com>
2014-07-03 01:52:35 +0800

    Initial commit.

---
+++ PartyAnimal/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  PartyAnimal
+//
+//  Created by 李偉誠 on 7/2/14.
+//  Copyright (c) 2014 Victoire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleCast/GoogleCast.h>
+
+@interface ViewController : UIViewController<GCKDeviceScannerListener,
+                                             GCKDeviceManagerDelegate,
+                                             GCKMediaControlChannelDelegate,
+                                             UIActionSheetDelegate>
+@end
---
+++ PartyAnimal/ViewController.m
@@ -0,0 +1,133 @@
+//
+//  ViewController.m
+//  PartyAnimal
+//
+//  Created by 李偉誠 on 7/2/14.
+//  Copyright (c) 2014 Victoire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+static NSString * kReceiverAppID;
+
+@interface ViewController () {
+    UIImage * _btnImage;
+    UIImage * _btnImageSelected;
+}
+
+@property GCKMediaControlChannel *mediaControlChannel;
+@property GCKApplicationMetadata *applicationMetadata;
+@property GCKDevice *selectedDevice;
+@property(nonatomic, strong) GCKDeviceScanner *deviceScanner;
+@property(nonatomic, strong) UIButton *chromecastButton;
+@property(nonatomic, strong) GCKDeviceManager *deviceManager;
+@property(nonatomic, readonly) GCKMediaInformation *mediaInformation;
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    kReceiverAppID = kGCKMediaDefaultReceiverApplicationID;
+    
+    // Create chromecast button
+    _btnImage = [UIImage imageNamed:@"icon-cast-identified.png"];
+    _btnImageSelected = [UIImage imageNamed:@"icon-cast-connect.png"];
+    
+    _chromecastButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [_chromecastButton addTarget:self action:@selector(chooseDevice:) forControlEvents:UIControlEventTouchDown];
+    _chromecastButton.frame = CGRectMake(0, 0, _btnImage.size.width, _btnImage.size.height);
+    [_chromecastButton setImage:nil forState:UIControlStateNormal];
+    _chromecastButton.hidden = YES;
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:_chromecastButton];
+    
+    // Initialize device scanner
+    self.deviceScanner = [[GCKDeviceScanner alloc]init];
+    
+    [self.deviceScanner addListener:self];
+    [self.deviceScanner startScan];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)chooseDevice:(id)sender {
+
+    if (self.selectedDevice == nil) {
+        // Choose device
+        UIActionSheet *sheet =
+            [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"Connect to Device", nil)
+                                        delegate:self
+                               cancelButtonTitle:nil
+                          destructiveButtonTitle:nil
+                               otherButtonTitles:nil];
+        
+        for (GCKDevice *device in self.deviceScanner.devices) {
+            [sheet addButtonWithTitle:device.friendlyName];
+        }
+        
+        [sheet addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
+        sheet.cancelButtonIndex = sheet.numberOfButtons - 1;
+    }
+    else {
+        [self updateStatsFromDevice];
+        
+        NSString *frendlyName = [NSString stringWithFormat:NSLocalizedString(@"Casting to %@", nil), self.selectedDevice.friendlyName];
+        NSString *mediaTitle = [self.mediaInformation.metadata stringForKey:kGCKMetadataKeyTitle];
+        
+        UIActionSheet *sheet = [[UIActionSheet alloc] init];
+        sheet.title = frendlyName;
+        sheet.delegate = self;
+        if (mediaTitle != nil) {
+            [sheet addButtonWithTitle:mediaTitle];
+        }
+        
+        //Offer disconnect option
+        [sheet addButtonWithTitle:@"Disconnect"];
+        [sheet addButtonWithTitle:@"Cancel"];
+        sheet.destructiveButtonIndex = (mediaTitle != nil ? 1 : 0);
+        sheet.cancelButtonIndex = (mediaTitle != nil ? 2 : 1);
+        
+        [sheet showInView:_chromecastButton];
+    }
+}
+
+- (void)updateStatsFromDevice {
+    
+    if (self.mediaControlChannel && self.isConnected) {
+        _mediaInformation = self.mediaControlChannel.mediaStatus.mediaInformation;
+    }
+}
+
+- (BOOL)isConnected {
+    return self.deviceManager.isConnected;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
