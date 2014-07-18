18c0b41777d84f3358c8964db1d0031fe5e6510c
msc0953 <msc0953@gmail.com>
2014-07-18 18:19:40 +0800

    Add error handle and cast vedio code.

--- PartyAnimal/ViewController.m
+++ PartyAnimal/ViewController.m
@@ -51,7 +51,7 @@ static NSString * kReceiverAppID = @"AE7CB007";
     [_chromecastButton addTarget:self action:@selector(chooseDevice:) forControlEvents:UIControlEventTouchDown];
     _chromecastButton.frame = CGRectMake(0, 0, _btnImage.size.width, _btnImage.size.height);
     [_chromecastButton setImage:_btnImage forState:UIControlStateNormal];
-    _chromecastButton.hidden = YES;
+    _chromecastButton.hidden = NO;
 
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:_chromecastButton];
     
@@ -131,36 +131,10 @@ static NSString * kReceiverAppID = @"AE7CB007";
     [self.deviceManager connect];
 }
 
-
-- (IBAction)castVideo:(id)sender {
-    
-    NSLog(@"Cast Video");
-    
-    //Show alert if not connected
-    if (!self.deviceManager || !self.deviceManager.isConnected) {
-        UIAlertView *alert =
-        [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Not Connected", nil)
-                                   message:NSLocalizedString(@"Please connect to Cast device", nil)
-                                  delegate:nil
-                         cancelButtonTitle:NSLocalizedString(@"OK", nil)
-                         otherButtonTitles:nil];
-        [alert show];
-        return;
-    }
-    
-    // Define Media metadata
-    
-}
-
-#pragma mark - GCKDeviceScannerListener
-- (void)deviceDidComeOnline:(GCKDevice *)device {
-    NSLog(@"device found!! %@", device.friendlyName);
-    [self updateButtonStates];
-}
-
-- (void)deviceDidGoOffline:(GCKDevice *)device {
-    NSLog(@"device disappeared!!!");
-    [self updateButtonStates];
+- (void) deviceDisconnected {
+    self.mediaControlChannel = nil;
+    self.deviceManager = nil;
+    self.selectedDevice = nil;
 }
 
 - (void)updateButtonStates {
@@ -186,15 +160,162 @@ static NSString * kReceiverAppID = @"AE7CB007";
     }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// Cast Vedio
+- (IBAction)castVideo:(id)sender {
+    
+    NSLog(@"Cast Video");
+    
+    //Show alert if not connected
+    if (!self.deviceManager || !self.deviceManager.isConnected) {
+        UIAlertView *alert =
+        [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Not Connected", nil)
+                                   message:NSLocalizedString(@"Please connect to Cast device", nil)
+                                  delegate:nil
+                         cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                         otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    //Define Media metadata
+    GCKMediaMetadata *metadata = [[GCKMediaMetadata alloc] init];
+    
+    [metadata setString:@"Big Buck Bunny (2008)" forKey:kGCKMetadataKeyTitle];
+    
+    [metadata setString:@"Big Buck Bunny tells the story of a giant rabbit with a heart bigger than "
+     "himself. When one sunny day three rodents rudely harass him, something "
+     "snaps... and the rabbit ain't no bunny anymore! In the typical cartoon "
+     "tradition he prepares the nasty rodents a comical revenge."
+                 forKey:kGCKMetadataKeySubtitle];
+    
+    [metadata addImage:[[GCKImage alloc]
+                        initWithURL:[[NSURL alloc] initWithString:@"http://commondatastorage.googleapis.com/"
+                                     "gtv-videos-bucket/sample/images/BigBuckBunny.jpg"]
+                        width:480
+                        height:360]];
+    
+    //define Media information
+    GCKMediaInformation *mediaInformation =
+    [[GCKMediaInformation alloc] initWithContentID:
+     @"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                                        streamType:GCKMediaStreamTypeNone
+                                       contentType:@"video/mp4"
+                                          metadata:metadata
+                                    streamDuration:0
+                                        customData:nil];
+    
+    //cast video
+    [_mediaControlChannel loadMedia:mediaInformation autoplay:TRUE playPosition:0];
 }
-*/
+
+#pragma mark - GCKDeviceScannerListener
+- (void)deviceDidComeOnline:(GCKDevice *)device {
+    NSLog(@"device found!! %@", device.friendlyName);
+    [self updateButtonStates];
+}
+
+- (void)deviceDidGoOffline:(GCKDevice *)device {
+    NSLog(@"device disappeared!!!");
+    [self updateButtonStates];
+}
+
+#pragma mark UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger) buttonIndex
+{
+    if (self.selectedDevice == nil) {
+        if (buttonIndex < self.deviceScanner.devices.count)
+        {
+            self.selectedDevice = self.deviceScanner.devices[buttonIndex];
+            NSLog(@"Selecting device:%@", self.selectedDevice.friendlyName);
+            [self connectToDevice];
+        }
+    }
+    else
+    {
+        if (buttonIndex == 1)
+        {
+            //Disconnect button
+            NSLog(@"Disconnecting device:%@", self.selectedDevice.friendlyName);
+            // New way of doing things: We're not going to stop the applicaton. We're just going
+            // to leave it.
+            [self.deviceManager leaveApplication];
+            // If you want to force application to stop, uncomment below
+            //[self.deviceManager stopApplicationWithSessionID:self.applicationMetadata.sessionID];
+            [self.deviceManager disconnect];
+            
+            [self deviceDisconnected];
+            [self updateButtonStates];
+        }
+        else if (buttonIndex == 0)
+        {
+            // Join the existing session.
+        }
+    }
+}
+
+#pragma mark - GCKDeviceManagerDelegate
+
+- (void)deviceManagerDidConnect:(GCKDeviceManager *)deviceManager {
+    NSLog(@"connected!!");
+    
+    [self updateButtonStates];
+    [self.deviceManager launchApplication:kReceiverAppID];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+didConnectToCastApplication:(GCKApplicationMetadata *)applicationMetadata
+            sessionID:(NSString *)sessionID
+  launchedApplication:(BOOL)launchedApplication {
+    
+    NSLog(@"application has launched");
+    self.mediaControlChannel = [[GCKMediaControlChannel alloc] init];
+    self.mediaControlChannel.delegate = self;
+    [self.deviceManager addChannel:self.mediaControlChannel];
+    [self.mediaControlChannel requestStatus];
+    
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+didFailToConnectToApplicationWithError:(NSError *)error {
+    [self showError:error];
+    
+    [self deviceDisconnected];
+    [self updateButtonStates];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+didFailToConnectWithError:(GCKError *)error {
+    [self showError:error];
+    
+    [self deviceDisconnected];
+    [self updateButtonStates];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager didDisconnectWithError:(GCKError *)error {
+    NSLog(@"Received notification that device disconnected");
+    
+    if (error != nil) {
+        [self showError:error];
+    }
+    
+    [self deviceDisconnected];
+    [self updateButtonStates];
+    
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+didReceiveStatusForApplication:(GCKApplicationMetadata *)applicationMetadata {
+    self.applicationMetadata = applicationMetadata;
+}
+
+#pragma mark - misc
+- (void)showError:(NSError *)error {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", nil)
+                                                    message:NSLocalizedString(error.description, nil)
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                          otherButtonTitles:nil];
+    [alert show];
+}
 
 @end
