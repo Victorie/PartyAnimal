9153de90a90b774e64702e6b7dbc78aee1af5040
msc0953 <msc0953@gmail.com>
2014-08-14 18:35:13 +0800

    Add New class that connect search view.

---
+++ PartyAnimal/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  PartyAnimal
+//
+//  Created by 李偉誠 on 8/14/14.
+//  Copyright (c) 2014 Victoire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
